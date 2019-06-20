008146a3800dbb5a7389cdc923737d5611abd3c0
m-hakuya <pws.sana.1213@gmail.com>
2019-06-20 10:58:32 +0900

    ファイルパス修正

--- notes_text_creator/manager/views.py
+++ notes_text_creator/manager/views.py
@@ -17,16 +17,15 @@ from django.views.decorators.csrf import csrf_exempt
 def CreateText(request):
 	form = cgi.FieldStorage()
 	title = request.POST.get('title')
+	filePath = os.getcwd()+'/'+title
 	path = os.getcwd()
-	if os.path.exists(title):
-		shutil.rmtree(title)
-	os.mkdir(title)
-
+	if os.path.exists(filePath):
+		shutil.rmtree(filePath)
+	os.mkdir(filePath)
 	wf = open(os.getcwd()+'/template/01.基本情報.txt')
 	data1 = wf.read()  # ファイル終端まで全て読んだデータを返す
 	wf.close()
 	
-
 	data1 = data1.replace("｛案件区分｝",request.POST.get('itemDivision'))
 	data1 = data1.replace("｛システム名｝",request.POST.get('systemName'))
 	data1 = data1.replace("｛作業場所｝",request.POST.get('workPlace'))
@@ -39,30 +38,31 @@ def CreateText(request):
 	data1 = data1.replace("｛対応担当者｝",request.POST.get('tantou'))	
 	data1 = data1.replace("｛関係者｝",request.POST.get('kankei'))
 #	shutil.copy(os.getcwd()+'/template/01.基本情報.txt', title+'/')
-	with codecs.open(title+'/01.txt','w', encoding='utf-8') as f:
+	with codecs.open(filePath+'/01.txt','w', encoding='utf-8') as f:
 		f.write(data1)
 		f.close()	
 		
-	with codecs.open(title+'/02.txt','w', encoding='utf-8') as f2:
+	with codecs.open(filePath+'/02.txt','w', encoding='utf-8') as f2:
 		f2.write(request.POST.get('content'))
 		f2.close()
 		
 	if request.POST.get('genin') != "":
-		with codecs.open(title+'/03.txt','w', encoding='utf-8') as f3:
+		with codecs.open(filePath+'/03.txt','w', encoding='utf-8') as f3:
 			f3.write(request.POST.get('genin'))
 			f3.close()
 			
-	with codecs.open(title+'/04.txt','w', encoding='utf-8') as f4:
+	with codecs.open(filePath+'/04.txt','w', encoding='utf-8') as f4:
 		f4.write(request.POST.get('taisaku'))
 		f4.close()			
-	shutil.make_archive(title, 'zip', root_dir= title)
+	shutil.make_archive(title, 'zip', root_dir= filePath)
 	
-	shutil.rmtree(title)
+	shutil.rmtree(filePath)
 	
-	file_path = os.path.join(settings.MEDIA_ROOT, title+'.zip')
+	file_path = os.path.join(settings.MEDIA_ROOT, filePath+'.zip')
 	if os.path.exists(file_path):
 		with open(file_path, 'rb') as fh:
 			response = HttpResponse(fh.read(), content_type="application/vnd.ms-excel")
-			response['Content-Disposition'] = 'inline; filename="{fn}"'.format(fn=urllib.parse.quote(title+'.zip'))
+			response['Content-Disposition'] = 'inline; filename="{fn}"'.format(fn=urllib.parse.quote(filePath+'.zip'))
 			return response
+			
 	raise Http404
